4fc13fbf2bf42c43432bbfa2e58573e30deb608e
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:28:12 +0530

    Update TreeRecursion.c

--- Recursion/TreeRecursion.c
+++ Recursion/TreeRecursion.c
@@ -1,1 +1,13 @@
 // TreeRecursion.c
+#include <stdio.h>
+void fun(int n){
+    if(n>0){
+        printf("%d",n);
+        fun(n-1);
+        fun(n-1);
+    }
+}
+int main() {
+  fun(3);
+  return 0;
+}
