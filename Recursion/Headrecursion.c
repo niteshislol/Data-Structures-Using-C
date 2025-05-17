9e9d6e903759a90887b591bba4f943d9d568a52a
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:26:17 +0530

    Update Headrecursion.c

--- Recursion/Headrecursion.c
+++ Recursion/Headrecursion.c
@@ -1,1 +1,14 @@
 // Headrecursion.c
+#include <stdio.h>
+void fun(int n){
+    int i = 1;
+    while(i<=n){
+        printf("%d",i);
+        i++;
+    }
+}
+int main() {
+    int x = 3;
+    fun(3);
+  return 0;
+}
