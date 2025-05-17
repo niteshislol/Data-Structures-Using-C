acfc1b575c25a4a29bc1d45d6cc51c4e9ed9d71c
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:26:59 +0530

    Update PowerUsingLoop.c

--- Recursion/PowerUsingLoop.c
+++ Recursion/PowerUsingLoop.c
@@ -1,1 +1,25 @@
 // PowerUsingLoop.c
+#include <stdio.h>
+int Power(int m, int n)
+{
+    int j = 1;
+    if (n == 1) return m;
+    for (int i = 1; i <= n; i++)
+    {
+        j = m * j;
+    }
+    return j;
+}
+int main()
+{
+    int x, y;
+    printf("Enter the Base: ");
+    scanf("%d", &x);
+
+    printf("Enter the Power: ");
+    scanf("%d", &y);
+
+    // Power(x,y);
+    printf("%d", Power(x, y));
+    return 0;
+}
