a05b5cdcdc23d8cd05643df6618c333e27adf325
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:25:35 +0530

    Update FactorialofN.c

--- Recursion/FactorialofN.c
+++ Recursion/FactorialofN.c
@@ -1,1 +1,14 @@
 // FactorialofN.c
+#include <stdio.h>
+int Sum(int n);
+int Sum(int n){
+    if (n==0 || n==1) return 1;
+else return n*Sum(n-1);
+}
+
+int main() {
+    int x = 5;
+    printf("%d\n",Sum(x));
+
+  return 0;
+}
