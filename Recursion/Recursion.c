b507f2d82e25c29a2b97a39dbdd5fd1da450f806
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:27:31 +0530

    Update Recursion.c

--- Recursion/Recursion.c
+++ Recursion/Recursion.c
@@ -1,1 +1,15 @@
 // Recursion.c
+#include <stdio.h>
+void fun1(int n){
+    if(n>0){
+        // printf("%d",n);
+        fun1(n-1);
+        printf("%d",n);
+    }
+}
+
+int main() {
+    int x=3;
+    fun1(x);
+  return 0;
+}
