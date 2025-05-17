c4d662904a88a6c85322473cbbf7fa89604fdc39
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:27:15 +0530

    Update PowerUsingRecursion.c

--- Recursion/PowerUsingRecursion.c
+++ Recursion/PowerUsingRecursion.c
@@ -1,1 +1,25 @@
 // PowerUsingRecursion.c
+#include <stdio.h>
+// Takes Some Time
+int Pow(int m , int n){
+    if(n==0) return 1;
+
+    return Pow(m,n-1)*m;
+}
+//Short Method
+int Power1(int m,int n){
+  if(n==0) return 1;
+  if(n%2==0) return Power1(m*m,n/2);
+
+  return m*Power1(m*m,(n-1)/2);
+}
+int main() {
+ int x, y;
+    printf("Enter the Base: ");
+    scanf("%d", &x);
+
+    printf("Enter the Power: ");
+    scanf("%d", &y);
+    printf("%d",Power1(x,y));
+  return 0;
+}
