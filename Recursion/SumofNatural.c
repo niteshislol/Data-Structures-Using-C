83d1cbbcd0522e583a88c15900d39aa08b4970e3
Nitesh! <168538454+niteshislol@users.noreply.github.com>
2025-05-17 16:27:53 +0530

    Update SumofNatural.c

--- Recursion/SumofNatural.c
+++ Recursion/SumofNatural.c
@@ -1,1 +1,23 @@
 // SumofNatural.c
+#include <stdio.h>
+int Sum(int n);
+int Sum(int n){
+    if (n==0 || n==1) return 1;
+else return n + Sum(n-1);
+}
+int Isum(int n){
+    int s = 0;
+    for(int i=1; i<=n;i++){
+        s = s+i; 
+    }
+            return s;
+
+}
+int main() {
+    int x = 5;
+    printf("%d\n",Sum(x));
+    int r = Isum(5);
+    printf("%d",r);
+
+  return 0;
+}
